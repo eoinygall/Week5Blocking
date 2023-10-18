b0313e30f70e294c55200f262134fd34b6e0659e
G00392003 <G00392003@atu.ie>
2023-10-18 10:06:08 +0100

    First part of the assigment using Rest Template

---
+++ src/test/java/ie/atu/Week5Blocking/Week5BlockingApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.Week5Blocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5BlockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
