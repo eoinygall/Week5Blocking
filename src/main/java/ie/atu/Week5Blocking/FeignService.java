ad81d8a9ef6c2710a6634beefabb8584ccd22b93
G00392003 <G00392003@atu.ie>
2023-10-18 10:42:41 +0100

    Part 2 using Open Feign

---
+++ src/main/java/ie/atu/Week5Blocking/FeignService.java
@@ -0,0 +1,24 @@
+package ie.atu.Week5Blocking;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeignService {
+
+    private final TodoClient todoClient;
+
+    public FeignService(TodoClient todoClient) {
+        this.todoClient = todoClient;
+    }
+
+    public TodoResponse fetchData() {
+        TodoResponse td = todoClient.fetchData();
+        System.out.println(td);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return td;
+    }
+}
---
+++ src/main/java/ie/atu/Week5Blocking/TodoResponse.java
@@ -0,0 +1,19 @@
+package ie.atu.Week5Blocking;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class TodoResponse {
+    @JsonProperty("userId")
+    private int userId;
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("completed")
+    private boolean completed;
+}
